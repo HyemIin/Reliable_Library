2fdcf1491ad42f8c559a65edcc0363f6e9b8468a
Harry <114489245+HyemIin@users.noreply.github.com>
2022-12-23 16:54:08 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from strategy.RSIStrategy import *
-import sys
-
-app = QApplication(sys.argv)
-
-rsi_strategy = RSIStrategy()
-rsi_strategy.start()
-
-app.exec_()
+number = input("휴대전화 번호 입력: ")
+num = number.split("-")[0]
+if num == "011":
+    com = "SKT"
+elif num == "016":
+    com = "KT"
+elif num == "019":
+    com = "LGU"
+else:
+    com = "알수없음"
+print(f"당신은 {com} 사용자입니다.")
